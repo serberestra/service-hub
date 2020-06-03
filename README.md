5e60c26534a2d435281aec9cd509f083d14742eb
markwartman1 <markwartman1@gmail.com>
2020-06-02 21:25:07 -0500

    created a few components with no HTML

--- README.md
+++
@@ -1,3 +0,0 @@
-Hello Testing
-
-Hello from Mark
---
+++ ServiceHub/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServiceAddComponent } from './components/service-add/service-add.component';
+import { ServiceEditComponent } from './components/service-edit/service-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    ServiceAddComponent,
+    ServiceEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
