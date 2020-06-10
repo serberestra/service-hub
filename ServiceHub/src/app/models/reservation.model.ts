8d2abefc4046094565d3de097db918aefdbaac2c
markwartman1 <markwartman1@gmail.com>
2020-06-09 19:40:33 -0500

    reservation model

---
+++ ServiceHub/src/app/models/reservation.model.ts
@@ -0,0 +1,14 @@
+export class Reservation {
+
+    constructor(
+        public id: number,
+        public bookBy: string,
+        public workerId: number,
+        public date: Date,
+        public status: string,
+        // Company( workers[] ) .....  I think this is what we need ?? ?? ?? ?????? 
+
+        // Company name ?
+        // Worker
+    ){}
+}
